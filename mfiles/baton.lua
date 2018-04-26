24792a273c02911041c4e5327db080d6f198d35a
Nicholas Scheurich <nick@scheurich.me>
2018-04-26 16:00:37 -0500

    Remove mfiles

--- mfiles/baton.lua
+++
@@ -1,8 +0,0 @@
-return {
-  name = "baton",
-  desc = "An input library that bridges the gap between keyboard and joystick controls and allows you to easily define and change controls on the fly",
-  repo = "tesselode/baton",
-  versions = {"1.0"},
-  tags = {"input"},
-  files = {"baton.lua"}
-}
