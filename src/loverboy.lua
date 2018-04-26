4e3cb0d06e780eea10cbea2ea535bc065665a04e
Nicholas Scheurich <nick@scheurich.me>
2018-04-26 16:25:13 -0500

    Inline update functionality

--- src/loverboy.lua
+++ src/loverboy.lua
@@ -10,11 +10,17 @@ local function print_version()
   os.exit(0)
 end
 
+local function update_mfiles()
+  util.print_bold("==> Updating metafiles cache...")
+  local cmd = "cd " .. MFILES_PATH .. "/.. && git pull"
+  os.execute(cmd)
+end
+
 cli:set_name("loverboy")
 cli:command("add", "Add a library to the current project"):file(util.abspath("add"))
 cli:command("info", "Display information about a library"):file(util.abspath("info"))
 cli:command("search", "Search available libraries"):file(util.abspath("search"))
-cli:command("update", "Update local metafiles cache"):file(util.abspath("update"))
+cli:command("update", "Update local metafiles cache"):action(function() update_mfiles() end)
 cli:flag("-v, --version", "Prints the program's version", print_version)
 
 local args, err = cli:parse(arg)
